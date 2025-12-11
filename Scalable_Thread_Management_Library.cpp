4f77e5a0455ae66f96ccc5f24815cac0d76a71e9
Eekshith3216 <eeshugangisetty@gmail.com>
2025-12-11 16:25:14 +0530

    Add files via upload

---
+++ Scalable_Thread_Management_Library.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+#include <functional>
+
+using namespace std;
+
+// Semaphore Class Implementation
+class Semaphore {
+private:
+    mutex mtx;
+    condition_variable cv;
+    int count;
+
+public:
+    Semaphore(int c = 0) : count(c) {}
+
+    void acquire() {
+        unique_lock<mutex> lock(mtx);
+        cv.wait(lock, [&]() { return count > 0; });
+        count--;
+    }
+
+    void release() {
+        unique_lock<mutex> lock(mtx);
+        count++;
+        cv.notify_one();
+    }
+};
+
+class ThreadPool {
+private:
+    vector<thread> workers;
+    queue<function<void()>> tasks;
+
+    mutex qlock;
+    condition_variable cv;
+    bool stop = false;
+
+public:
+
+    ThreadPool(int nThreads) {
+        for (int i = 0; i < nThreads; i++) {
+            workers.emplace_back([this]() { workerThread(); });
+        }
+    }
+
+void workerThread() {
+        while (true) {
+            function<void()> task;
+
+            {   // Acquire lock
+                unique_lock<mutex> lock(qlock);
+
+                cv.wait(lock, [&]() { return stop || !tasks.empty(); });
+
+                if (stop && tasks.empty()) return;
+
+                task = tasks.front();
+                tasks.pop();
+            }
+
+            task(); 
+        }
+    }
+
+
+
+
+
